170404efd26ac1405bbc0bf334565cdf7045de68
George <49111202+Georgefwm@users.noreply.github.com>
2022-04-05 13:06:06 +1200

    Update: comments/formatting

--- src/ass1/MParallelSorter2.java
+++ src/ass1/MParallelSorter2.java
@@ -11,17 +11,6 @@ public class MParallelSorter2 implements Sorter{
 
 
 	/**
-	 * The benefit of this algorithm is that it is takes advantage of multiple threads. Implementation with CompletableFutures is
-	 * slightly different then Futures. CompletableFutures.supplyAsync() does all the work, whereas with Futures you have to set up
-	 * the thread pool yourself, and choose the type that is best suited for the nature of the tasks you are getting it to do.
-	 * There are some differences between the .cancel() methods, I have not used them so there is not much difference in the code
-	 * except the syntax.
-	 * <p>
-	 * 
-	 * I learned how to implement CompletableFutures to delegate a task to a 'worker'. Again, I was reminded of the importance 
-	 * of ordering when delegating tasks to new threads.
-	 * <p>
-	 * 
 	 * @param list to sort.
 	 * @return The sorted list.
 	 */
